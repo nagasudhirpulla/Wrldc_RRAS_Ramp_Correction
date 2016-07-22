bb7da43ddd7780ce2c3a4655f7b8ec28680d02fc
nagasudhirpulla <nagasudhirpulla@gmail.com>
2016-07-23 01:24:38 +0530

    presenting the bulk rras over, coloring pending

--- bulk-solve/index-rras.js
+++ bulk-solve/index-rras.js
@@ -1,5 +1,5 @@
 var rrasSolver = new RRASsolver();
-
+var solvedRRASAll = {};
 window.onload = function () {
     var fileInput = document.getElementById('fileInput');
     var fileDisplayArea = document.getElementById('fileDisplayArea');
@@ -17,4 +17,14 @@ function doRRASSolve() {
     var considerBlk = document.getElementById("consider_blk_input").value;
     var solveBlk = document.getElementById("solve_blk_input").value;
     solvedRRASAll = rrasSolver.solve(considerBlk, solveBlk);
+    //present the solved results in the the textArea fileDisplayArea
+    var str = "";
+    var gens = Object.keys(solvedRRASAll);
+    for (var i = 0; i < gens.length; i++) {
+        str += gens[i] + "\t" + "UP" + "\t";
+        str += solvedRRASAll[gens[i]]["UP"].join("\t") + "\n";
+        str += gens[i] + "\t" + "DOWN" + "\t";
+        str += solvedRRASAll[gens[i]]["DOWN"].join("\t") + "\n";
+    }
+    document.getElementById("fileDisplayArea").value = str;
 }
--- bulk-solve/rras-solver.js
+++ bulk-solve/rras-solver.js
@@ -291,6 +291,27 @@ function RRASsolver() {
         return {solvedRRASUp: solvedRRASUp, solvedRRASDown: solvedRRASDown};
     }
 
+    function trendConstraintSolve(val, change, prevVal) {
+        var result;
+        result = val;
+        if (change) {
+            result = val + change;
+        }
+        if (prevVal) {
+            //follow trend => val <= max(val, prevVal)
+            result = (result > Math.max(val, prevVal)) ? Math.max(val, prevVal) : result;
+        }
+        result = (result < 0) ? 0 : result;
+        return result;
+    }
+    /*
+     //tests
+     console.log(trendConstraintSolve(70, -80, 140) == 0);
+     console.log(trendConstraintSolve(70, 30, 140) == 100);
+     console.log(trendConstraintSolve(70, -30, 140) == 40);
+     console.log(trendConstraintSolve(70, 80, 140) == 140);
+     */
+
     function solveRRASBlkConstraints(isgsVal, isgsValPrev, newRRASUp, newRRASUpPrev, newRRASDown, newRRASDownPrev, rUpVal, rDnVal, dcVal, tmVal) {
         var RUPFlag = false;
         var RDNFlag = false;
